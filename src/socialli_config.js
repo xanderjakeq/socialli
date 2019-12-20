1cdef95ca05afc61a12df8e5193b1248b180f071
Xander Jake de los Santos <13279523+xanderjakeq@users.noreply.github.com>
2019-12-20 15:30:35 -0500

    Update socialli_config.js

--- src/socialli_config.js
+++ src/socialli_config.js
@@ -1,6 +1,6 @@
 const config = {
-	host: "xanderjakeq.id.blockstack",
-	instance_name: "Socialli",
+	host: "xanderjakeq2.id.blockstack",
+	instance_name: "Socialli test",
 	instance_description: "Socialli.st is a socialli instance for everyone."
 }
 
